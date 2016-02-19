e4d144d1ca674f7d41b97d09a78ef8c7d9c887f1
Anonymous <anonymous@noemail.com>
2016-02-19 03:31:09 +0000

    init

---
+++ src/main/java/com/puresoft/JavaSpringRestIotHomeApplication.java
@@ -0,0 +1,12 @@
+package com.puresoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRestIotHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringRestIotHomeApplication.class, args);
+	}
+}
